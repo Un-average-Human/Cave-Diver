ed0f744c27677469bb80ac33bf0335651c16c417
Un-average-Human <miguelmfernandes1008@gmail.com>
2026-02-28 19:50:59 +1300

    Add files via upload

---
+++ character_controller/control.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var color_rect: ColorRect = $CanvasLayer/ColorRect
+@onready var _1_st_label: Label = $"CanvasLayer/ColorRect/1st_label"
+@onready var _2_nd_label: Label = $"CanvasLayer/ColorRect/2nd_label"
+@onready var _3_rd_label: Label = $"CanvasLayer/ColorRect/3rd_label"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_start()
+
+
+func _start():
+	_1_st_label.show()
+	await get_tree().create_timer(5).timeout
+	_1_st_label.hide()
+	_2_nd_label.show()
+	await get_tree().create_timer(5).timeout
+	_2_nd_label.hide()
+	_3_rd_label.show()
+	await get_tree().create_timer(5).timeout
+	_3_rd_label.hide()
+	color_rect.hide()
+	get_tree().change_scene_to_file("res://test.tscn")
